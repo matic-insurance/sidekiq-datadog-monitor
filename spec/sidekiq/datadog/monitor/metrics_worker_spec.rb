7a288160c82e4107cc9ff6b153c32418141a68b7
DanPolyuha <dpolyuha@gmail.com>
2022-06-22 17:59:51 +0300

    Update for using with dogstatsd-ruby ver. 5+

--- spec/sidekiq/datadog/monitor/metrics_worker_spec.rb
+++ spec/sidekiq/datadog/monitor/metrics_worker_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Sidekiq::Datadog::Monitor::MetricsWorker do
   let(:perform) { described_class.new.perform }
-  let(:statsd) { instance_double(Datadog::Statsd, gauge: true) }
+  let(:statsd) { instance_double(Datadog::Statsd, gauge: true, close: true) }
   let(:stats) { instance_double(Sidekiq::Stats) }
   let(:stats_queue) { instance_double(Sidekiq::Queue) }
 
@@ -13,8 +13,7 @@ RSpec.describe Sidekiq::Datadog::Monitor::MetricsWorker do
       agent_port: 8125,
       tags: ['tag:tag', 'env:production'],
       queue: 'critical',
-      cron: '*/30 * * * *',
-      batch: batch
+      cron: '*/30 * * * *'
     }
   end
 
@@ -32,33 +31,11 @@ RSpec.describe Sidekiq::Datadog::Monitor::MetricsWorker do
     perform
   end
 
-  context 'when the batch mode is off' do
-    let(:batch) { false }
-
-    it 'posts queue size' do
-      expect(statsd).to have_received(:gauge).with('sidekiq.queue.size', 100, { tags: tags })
-    end
-
-    it 'posts queue latency' do
-      expect(statsd).to have_received(:gauge).with('sidekiq.queue.latency', 5000, { tags: tags })
-    end
+  it 'posts queue size' do
+    expect(statsd).to have_received(:gauge).with('sidekiq.queue.size', 100, { tags: tags })
   end
 
-  context 'when the batch mode is on' do
-    let(:batch) { true }
-
-    let(:statsd) do
-      instance_double(Datadog::Statsd, gauge: true).tap do |s|
-        allow(s).to receive(:batch).and_yield(s)
-      end
-    end
-
-    it 'posts queue size' do
-      expect(statsd).to have_received(:gauge).with('sidekiq.queue.size', 100, { tags: tags })
-    end
-
-    it 'posts queue latency' do
-      expect(statsd).to have_received(:gauge).with('sidekiq.queue.latency', 5000, { tags: tags })
-    end
+  it 'posts queue latency' do
+    expect(statsd).to have_received(:gauge).with('sidekiq.queue.latency', 5000, { tags: tags })
   end
 end
